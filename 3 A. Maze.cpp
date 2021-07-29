c02553f14c7bf751aa17ad20d5be8461f8408a33
Manrick Leal <55140949+rickleal@users.noreply.github.com>
2021-07-29 16:52:04 -0300

    Create 3 A. Maze.cpp

---
+++ 3 A. Maze.cpp
@@ -0,0 +1,51 @@
+// é preciso ter cuidado:
+// 1.Se quisermos deixar o labirinto ainda conectado, podemos simplesmente excluir a unidade que não tem seu filho
+// O fim do 2.dfs é a unidade que não tem seu filho
+// 3.Coloque o "X" nele!
+  
+  
+#include<iostream>
+using namespace std;
+int dx[4] = { 0,0,1,-1 };
+int dy[4] = { 1,-1,0,0 };
+int n = 0, m = 0, k = 0;
+bool st[501][501] = { 0 };
+char map[501][501] = { 0 };
+bool dfs(int x, int y) {
+	st[x][y] = 1;
+	for (int i = 0; i < 4; i++)
+	{
+		int x1 = x + dx[i];
+		int y1 = y + dy[i];
+		if (!st[x1][y1] && map[x1][y1] == '.')
+		{
+			dfs(x1, y1);
+		}
+	}
+	if (k > 0) {
+		map[x][y] = 'X';
+		k--;
+	}
+	return false;
+}
+int main() {
+	scanf("%d%d%d", &n, &m, &k);
+	for (int i = 0; i < n; ++i)
+		scanf("%s",map[i]);
+	int ti = 0, tj = 0;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+			if (map[i][j] == '.')
+			{
+				dfs(i, j);
+				for (int i = 0; i < n; ++i) {
+					for (int j = 0; j < m; ++j)
+						printf("%c", map[i][j]);
+					printf("\n");
+				}
+				return 0;
+			}
+	}
+	return 0;
+}
